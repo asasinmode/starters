e6e9ee00c7c2ccc99aae881d127198870d998734
asasinmode <skin9470@gmail.com>
2023-01-28 19:04:46 +0100

    update eslint config

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,4 +1,4 @@
-import { type Component, createApp } from 'vue';
+import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { router } from './router';
 import App from './App.vue';
@@ -8,7 +8,7 @@ import './assets/index.css';
 import 'uno.css';
 
 const pinia = createPinia();
-const app = createApp(App as Component);
+const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
